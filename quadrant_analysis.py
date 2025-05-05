a988e7afdfbb3d32dc811bbc56aa91776b3ee924
lnionescu <nionescu@student.ethz.ch>
2025-05-05 11:19:29 +0200

    just to have it here

---
+++ quadrant_analysis.py
@@ -0,0 +1,14 @@
+import numpy as np
+from skimage.io import imread
+
+'''
+splits the image in four quadrants to quantify the spatial variation of the degree of protein aggregation
+'''
+
+
+
+
+
+
+
+
