51006a6c342c20fb9df91aa90d86d33ad5813131
lnionescu <nionescu@student.ethz.ch>
2025-05-08 14:49:41 +0200

    starting

---
+++ time_dependence.py
@@ -0,0 +1,15 @@
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+import bioformats
+
+'''
+should be able to extract all necessary data from a .nd2 file: ie separate z-slices and channels
+test this for one mutant before i get to the full automation script
+make a local copy of all available timestamps of one mutant, say E3Q
+'''
+
+
+
+
+
