fda302d3e23edcfc4795164abaea265c95a66a2e
lnionescu <nionescu@student.ethz.ch>
2025-05-08 14:54:28 +0200

    give up dream of local copies

--- time_dependence.py
+++ time_dependence.py
@@ -6,7 +6,6 @@ import bioformats
 '''
 should be able to extract all necessary data from a .nd2 file: ie separate z-slices and channels
 test this for one mutant before i get to the full automation script
-make a local copy of all available timestamps of one mutant, say E3Q
 '''
 
 
