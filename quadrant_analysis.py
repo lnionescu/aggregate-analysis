6e9fd34f569c7e308e2af494b47af1e0739124ac
lnionescu <nionescu@student.ethz.ch>
2025-05-10 14:36:53 +0200

    get_quadrants added

--- quadrant_analysis.py
+++ quadrant_analysis.py
@@ -2,6 +2,8 @@ import numpy as np
 from skimage.io import imread
 import seaborn as sns
 import matplotlib.pyplot as plt
+from concentration_analysis import cell_metrics
+from cells_with_foci import cells_with_foci 
 
 '''
 splits the image in four quadrants to quantify the spatial variation of the degree of protein aggregation
@@ -16,20 +18,49 @@ def get_quadrants(fluorescent_img_path):
     '''
 
     fluorescent_img = imread(fluorescent_img_path)
+    rows, columns = fluorescent_img.shape
+
+    # extract the four quadrants
+    top_left = fluorescent_img[:rows//2, :columns//2]
+    top_right = fluorescent_img[:rows//2, columns//2:]
+    bottom_left = fluorescent_img[rows//2:, :columns//2]
+    bottom_right = fluorescent_img[:rows//2, columns//2:]
+
+    quadrants = {'top_left': top_left, 'top_right': top_right, 'bottom_left': bottom_left, 'bottom_right': bottom_right}
 
     return quadrants
-    pass
 
-def get_metrics(quadrants):
+def get_metrics(quadrants, fluorescent_img_path, brightfield_img_path):
     '''
     input: numpy arrays corresponding to the four quadrnts of the image under analysis
     purpose: using concentration_analysis (which already does all the segmentation and aggregate detection and whatever), extract the mean cell intensity and the
     cell variance (including foci)
+    gets all metrics for the entire image and then assigns cells to quadrants based on the coordinates of their masks
     output: average of metrics within each quadrant???
     '''
 
-    return summary
-    pass
+    # use concentration_analysis.py to get all metrics for all cells in the image (can be changed later to only extract mean and variance, in case this is too slow)
+    cells_with_aggregates, cell_masks = cells_with_foci(brightfield_path, fluorescent_path)
+    all_metrics = cell_metrics(fluorescent_path, cell_masks, cells_with_aggregates)
+    
+    # initialize dictionary for cell metrics separated by quadrant
+    quadrant_metrics = {'top_left': [], 'top_right': [], 'bottom_left': [], 'bottom_right': []}
+
+    # assign cells to quadrants based on the coordinates of the cell masks
+    for cell_id, metrics in all_metrics.items():
+        # get mask for current cell
+        cell_mask = cell_masks == cell_id
+        # get coordinates as the coordinates of the cell_mask matrix where cell_id is True (corresponds to coordinates of pixels belonging to the current cell)
+        cell_coords = np.argwhere(cell_mask)
+       
+        # check if current cell belongs to each quadrant
+        
+    
+
+
+
+    return quadrant_metrics
+
 
 def plot_metrics(summary):
     '''
