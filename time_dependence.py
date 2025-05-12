4ce8eb3a29a8c094d0fe77156c6d53dbfab72771
lnionescu <nionescu@student.ethz.ch>
2025-05-12 13:14:50 +0200

    organization

--- time_dependence.py
+++ time_dependence.py
@@ -9,14 +9,18 @@ test this for one mutant before i get to the full automation script
 '''
 
 from quadrant_analysis import get_quadrants, get_quadrant_metrics
-def plot_time_evolution(nd2_file_path):
- # open the entire nd2 file
-   with nd2.ND2File(nd2_file_path) as nd2_file:
+def get_time_evolution(nd2_file_path):
+    '''
+    input: path to nd2 file
+    output: dictionary containing the time evolution of some metric for each quadrant
+    '''
+    # open the entire nd2 file
+    with nd2.ND2File(nd2_file_path) as nd2_file:
         num_series = nd2_file.sizes.get('S', 1)
         time_points = range(num_series)
         print(time_points)
  
-       # dictionary with time evolution of chosen metric for each quadrant
+        # dictionary with time evolution of chosen metric for each quadrant
         time_evolution = {'top_left': [], 'top_right': [], 'bottom_left': [], 'bottom_right': []}
   
   
