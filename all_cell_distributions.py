d9bd1772aa0e0c627c031d1d5457278d3e150c01
lnionescu <nionescu@student.ethz.ch>
2025-05-12 15:03:24 +0200

    main functions and logic

--- all_cell_distributions.py
+++ all_cell_distributions.py
@@ -3,16 +3,42 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from concentration_analysis import cell_metrics
 from cells_with_foci import cells_with_foci
+import os
 ''' 
 uses dictionary of cell metrics to get the distribution of relative standard deviation of cell intensities
 '''
 
+def get_distribution(cell_measurements):
 
-fluorescent_img_path = '/Users/nataliaionescu/Documents/PKM2/pngs_for_experimenting/E3Q_t0_fluorescent.png' 
-brightfield_img_path = '/Users/nataliaionescu/Documents/PKM2/pngs_for_experimenting/E3Q_t0_brightfield.png' 
 
-cells_with_aggregates, cell_masks, _ = cells_with_foci(brightfield_img_path, fluorescent_img_path)
-cell_measurements = cell_metrics(fluorescent_img_path, cell_masks, cells_with_aggregates)
+
+    return metric
+
+
+
+
+
+
+
+
+
+def plot_distribution(metric):
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    fluorescent_img_path = '/Users/nataliaionescu/Documents/PKM2/pngs_for_experimenting/E3Q_t0_fluorescent.png' 
+    brightfield_img_path = '/Users/nataliaionescu/Documents/PKM2/pngs_for_experimenting/E3Q_t0_brightfield.png' 
+
+    cells_with_aggregates, cell_masks, _ = cells_with_foci(brightfield_img_path, fluorescent_img_path)
+    cell_measurements = cell_metrics(fluorescent_img_path, cell_masks, cells_with_aggregates)
 
 
 
