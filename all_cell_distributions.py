a46e5e5317dd3b5d8a3eb14b09e9077b92b9147c
lnionescu <nionescu@student.ethz.ch>
2025-05-12 15:46:44 +0200

    rsd calculation and plotting

--- all_cell_distributions.py
+++ all_cell_distributions.py
@@ -4,33 +4,42 @@ import seaborn as sns
 from concentration_analysis import cell_metrics
 from cells_with_foci import cells_with_foci
 import os
+
 ''' 
 uses dictionary of cell metrics to get the distribution of relative standard deviation of cell intensities
 '''
 
 def get_distribution(cell_measurements):
+    '''
+    input: dictionary of cell measurements obtained using cell_metrics
+    computes the relative standard deviation (sd/mean) for the intensity of each cell
+    output: dictionary of cell RSDs
+    '''
+
+    rsd_values = {}     # initialize dict for rsd values
+
+    # get mean and standard deviation of intensity within each cell
+    for cell_id, metrics in cell_measurements.items():
+        mean_intensity = metrics.get('cell_mean_intensity')
+        sd_intensity = metrics.get('cell_sd_intensity')
+        if mean_intensity > 0:
+            rsd = sd_intensity / mean_intensity
+            rsd_values[cell_id] = rsd
+
+    return rsd_values
 
 
 
-    return metric
+def plot_distribution(rsd_values):
+    '''
+    input: dictionary of rsd values of all cells in the image under analysis
+    '''
 
-
-
-
-
-
-
-
-
-def plot_distribution(metric):
-
-
-
-
-
-
-
-
+    plt.bar(rsd_values.keys(), rsd_values.values())
+    plt.xlabel('Cell index')
+    plt.ylabel('RSD of intensity')
+    plt.title('Distribution of relative standard deviations of cell intensities')
+    plt.show()
 
 
 if __name__ == '__main__':
@@ -39,7 +48,8 @@ if __name__ == '__main__':
 
     cells_with_aggregates, cell_masks, _ = cells_with_foci(brightfield_img_path, fluorescent_img_path)
     cell_measurements = cell_metrics(fluorescent_img_path, cell_masks, cells_with_aggregates)
-
+    rsd_values = get_distribution(cell_measurements)
+    plot_distribution(rsd_values)
 
 
 
