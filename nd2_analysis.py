c5f0d6506dc443cd8ec5bf352c76d350eb666749
lnionescu <nionescu@student.ethz.ch>
2025-05-13 18:18:58 +0200

    rationale

---
+++ nd2_analysis.py
@@ -0,0 +1,23 @@
+import numpy as np
+import os
+import csv
+import nd2
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from concentration_analysis import cell_metrics, save_measurements_to_csv
+
+'''
+this doesn't have any extra functions, it is simply meant to process nd2 files and save the measurements of their cells as a csv file
+'''
+
+# start by understanding the dimensions of the nd2 files and how to extract what i need to apply previous methods
+
+nd2_file_path = '/Users/nataliaionescu/Documents/PKM2/PKM2_E3Q_ChannelBrightfield,GFP_Seq0001.nd2' 
+
+
+
+
+
+
+
