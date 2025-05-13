6b2cd0e0418b092e30d36f431c22ff13a2149a66
lnionescu <nionescu@student.ethz.ch>
2025-05-13 19:33:36 +0200

    can read nd2 dimensions

--- nd2_analysis.py
+++ nd2_analysis.py
@@ -13,8 +13,15 @@ this doesn't have any extra functions, it is simply meant to process nd2 files a
 
 # start by understanding the dimensions of the nd2 files and how to extract what i need to apply previous methods
 
-nd2_file_path = '/Users/nataliaionescu/Documents/PKM2/PKM2_E3Q_ChannelBrightfield,GFP_Seq0001.nd2' 
+nd2_file_path = '/Users/nataliaionescu/Documents/PKM2/PKM2_WT_ChannelBrightfield,GFP_Seq0000.nd2' 
 
+def process_nd2_file(nd2_file_path):
+    with nd2.ND2File(nd2_file_path) as nd2_file:
+        metadata = nd2_file.metadata
+        dimensions = nd2_file.sizes
+        print(dimensions)
+
+process_nd2_file(nd2_file_path)
 
 
 
