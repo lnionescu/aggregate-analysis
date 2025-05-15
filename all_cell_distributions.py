3fe1dde53e000e675ec3482b7c892a6ebde02e2e
lnionescu <nionescu@student.ethz.ch>
2025-05-15 10:53:09 +0200

    todo

--- all_cell_distributions.py
+++ all_cell_distributions.py
@@ -4,19 +4,24 @@ import seaborn as sns
 from concentration_analysis import cell_metrics
 from cells_with_foci import cells_with_foci
 import os
+import csv
 
 ''' 
 uses dictionary of cell metrics to get the distribution of relative standard deviation of cell intensities
 '''
 
-def get_distribution(cell_measurements):
+def get_distribution(cell_measurements, output_folder):
     '''
     input: dictionary of cell measurements obtained using cell_metrics
     computes the relative standard deviation (sd/mean) for the intensity of each cell
     output: dictionary of cell RSDs
     '''
 
+    # TODO modify this function such that it takes a csv file as argument
+
     rsd_values = {}     # initialize dict for rsd values
+    os.makedirs(output_folder, exist_ok=True)
+    output_file = os.path.join(output_folder, 'rsd_values.csv')
 
     # get mean and standard deviation of intensity within each cell
     for cell_id, metrics in cell_measurements.items():
@@ -25,6 +30,13 @@ def get_distribution(cell_measurements):
         if mean_intensity > 0:
             rsd = sd_intensity / mean_intensity
             rsd_values[cell_id] = rsd
+    
+    with open(output_file, mode='w', newline='') as csv_file:
+        writer = csv.writer(csv_file)
+        writer.writerow(['Cell Index', 'RSD'])   # table header
+        for cell_id, rsd in rsd_values.items():
+            writer.writerow([cell_id, rsd])
+
 
     return rsd_values
 
@@ -48,7 +60,8 @@ if __name__ == '__main__':
 
     cells_with_aggregates, cell_masks, _ = cells_with_foci(brightfield_img_path, fluorescent_img_path)
     cell_measurements = cell_metrics(fluorescent_img_path, cell_masks, cells_with_aggregates)
-    rsd_values = get_distribution(cell_measurements)
+    output_folder = 'rsd_results'
+    rsd_values = get_distribution(cell_measurements, output_folder)
     plot_distribution(rsd_values)
 
 
