a2700c9755ef55b6cf79c7f507843231e9ab528f
lnionescu <nionescu@student.ethz.ch>
2025-06-08 13:52:21 +0200

    plots for all days and plots with log values subtracte

--- foci_fraction.py
+++ foci_fraction.py
@@ -19,31 +19,128 @@ def calculate_foci_fraction(csv_path):
     fraction = cells_with_foci / total_cells if total_cells > 0 else 0
     return fraction
 
-# file paths for the cell measurements of each mutant (change depending on day)
-file_paths = ['/Users/nataliaionescu/Documents/PKM2/jobs_results/log/E3Q_all_data/Series1_Z1_fluorescent_cell_measurements.csv', '/Users/nataliaionescu/Documents/PKM2/jobs_results/log/Q10E_all_data/Series1_Z1_fluorescent_cell_measurements.csv', '/Users/nataliaionescu/Documents/PKM2/jobs_results/log/WT_all_data/Series1_Z1_fluorescent_cell_measurements.csv']   
+# file paths for each day and mutant, write all of them in order to subtract the baseline values
+# currently doing: series 1, z-slice 1
+day1_paths = ['/Users/nataliaionescu/Documents/PKM2/jobs_results/day1/E3Q_all_data/Series1_Z1_fluorescent.png_cell_measurements.csv', '/Users/nataliaionescu/Documents/PKM2/jobs_results/day1/Q10E_all_data/Series1_Z1_fluorescent_cell_measurements.csv', '/Users/nataliaionescu/Documents/PKM2/jobs_results/day1/WT_all_data/Series1_Z1_fluorescent_cell_measurements.csv']
+
+day2_paths = ['/Users/nataliaionescu/Documents/PKM2/jobs_results/day2/E3Q_all_data/Series1_Z1_fluorescent_cell_measurements.csv', '/Users/nataliaionescu/Documents/PKM2/jobs_results/day2/Q10E_all_data/Series1_Z1_fluorescent_cell_measurements.csv', '/Users/nataliaionescu/Documents/PKM2/jobs_results/day2/WT_all_data/Series1_Z1_fluorescent_cell_measurements.csv']
+
+day3_paths = ['/Users/nataliaionescu/Documents/PKM2/jobs_results/day3/E3Q_all_data/Series1_Z1_fluorescent_cell_measurements.csv', '/Users/nataliaionescu/Documents/PKM2/jobs_results/day3/Q10E_all_data/Series1_Z1_fluorescent_cell_measurements.csv', '/Users/nataliaionescu/Documents/PKM2/jobs_results/day3/WT_all_data/Series1_Z1_fluorescent_cell_measurements.csv']
+
+log_paths = ['/Users/nataliaionescu/Documents/PKM2/jobs_results/log/E3Q_all_data/Series1_Z1_fluorescent_cell_measurements.csv', '/Users/nataliaionescu/Documents/PKM2/jobs_results/log/Q10E_all_data/Series1_Z1_fluorescent_cell_measurements.csv', '/Users/nataliaionescu/Documents/PKM2/jobs_results/log/WT_all_data/Series1_Z1_fluorescent_cell_measurements.csv']   
+
 protein_names = ['E3Q', 'Q10E', 'WT']
-fractions = []
-
-for file_path in file_paths:
-    fraction = calculate_foci_fraction(file_path)
-    fractions.append(fraction)
-    print(f'Fraction of cells with foci {fraction:.3f}')
-
-# plot
-plt.figure(figsize=(8,6))
 colors = ['blue', 'orange', 'green']
-bars = plt.bar(protein_names, fractions, color=colors, edgecolor='black')
 
+# fractions for each day
+day1_fractions = [calculate_foci_fraction(path) for path in day1_paths]
+day2_fractions = [calculate_foci_fraction(path) for path in day2_paths]
+day3_fractions = [calculate_foci_fraction(path) for path in day3_paths]
+log_fractions = [calculate_foci_fraction(path) for path in log_paths]
+
+# plot day 1 
+plt.figure(figsize=(8,6))
+bars = plt.bar(protein_names, day1_fractions, color=colors, edgecolor='black')
 x = range(len(protein_names))
 plt.xlabel('Protein')
 plt.ylabel('Fraction of cells containing foci')
 ax = plt.gca()
-#ax.set_ylim([1.2, 1.4])
-plt.title('Log: Comparison of fractions of cells containing foci')
+plt.title('Day 1: Fraction of cells containing foci')
 plt.xticks(x, protein_names)
-
 plt.tight_layout()
 plt.show()
+
+# plot day 2
+plt.figure(figsize=(8,6))
+bars = plt.bar(protein_names, day2_fractions, color=colors, edgecolor='black')
+x = range(len(protein_names))
+plt.xlabel('Protein')
+plt.ylabel('Fraction of cells containing foci')
+ax = plt.gca()
+plt.title('Day 2: Fraction of cells containing foci')
+plt.xticks(x, protein_names)
+plt.tight_layout()
+plt.show()
+
+# plot day 3
+plt.figure(figsize=(8,6))
+bars = plt.bar(protein_names, day3_fractions, color=colors, edgecolor='black')
+x = range(len(protein_names))
+plt.xlabel('Protein')
+plt.ylabel('Fraction of cells containing foci')
+ax = plt.gca()
+plt.title('Day 3: Fraction of cells containing foci')
+plt.xticks(x, protein_names)
+plt.tight_layout()
+plt.show()
+
+# plot log
+plt.figure(figsize=(8,6))
+bars = plt.bar(protein_names, log_fractions, color=colors, edgecolor='black')
+x = range(len(protein_names))
+plt.xlabel('Protein')
+plt.ylabel('Fraction of cells containing foci')
+ax = plt.gca()
+plt.title('Log: Fraction of cells containing foci')
+plt.xticks(x, protein_names)
+plt.tight_layout()
+plt.show()
+
+# get differences day - log
+day1_diff = [day1_fractions[i] - log_fractions[i] for i in range(3)]
+day2_diff = [day2_fractions[i] - log_fractions[i] for i in range(3)]
+day3_diff = [day3_fractions[i] - log_fractions[i] for i in range(3)]
+
+# plot day 1 - log
+plt.figure(figsize=(8,6))
+bars = plt.bar(protein_names, day1_diff, color=colors, edgecolor='black')
+x = range(len(protein_names))
+plt.xlabel('Protein')
+plt.ylabel('Fraction of cells containing foci')
+ax = plt.gca()
+plt.title('Day 1: Fraction of cells containing foci, subtracting baseline values')
+plt.xticks(x, protein_names)
+plt.tight_layout()
+plt.show()
+
+# plot day 2 - log
+plt.figure(figsize=(8,6))
+bars = plt.bar(protein_names, day2_diff, color=colors, edgecolor='black')
+x = range(len(protein_names))
+plt.xlabel('Protein')
+plt.ylabel('Fraction of cells containing foci')
+ax = plt.gca()
+plt.title('Day 2: Fraction of cells containing foci, subtracting baseline values')
+plt.xticks(x, protein_names)
+plt.tight_layout()
+plt.show()
+
+# plot day 3 - log
+plt.figure(figsize=(8,6))
+bars = plt.bar(protein_names, day3_diff, color=colors, edgecolor='black')
+x = range(len(protein_names))
+plt.xlabel('Protein')
+plt.ylabel('Fraction of cells containing foci')
+ax = plt.gca()
+plt.title('Day 3: Fraction of cells containing foci, subtracting baseline values')
+plt.xticks(x, protein_names)
+plt.tight_layout()
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
   
 
