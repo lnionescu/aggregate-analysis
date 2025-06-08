c8a81ec7a31bdb1a9a0aff1de1e74c44580f0f83
lnionescu <nionescu@student.ethz.ch>
2025-06-08 18:22:42 +0200

    series loop and baseline subtraction

--- foci_fraction.py
+++ foci_fraction.py
@@ -2,131 +2,240 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 
-'''
-Input: csv files of the cell measurements for each mutant on the desired day
-Output: bar plot showing the fractions of cells with foci for each mutant
-'''
-
 def calculate_foci_fraction(csv_path):
-    '''
-    calculates fraction of cells containing foci from the cell measurements csv file
-    returns: cells_with_foci / total_cells fraction
-    '''
-    # read csv of cell measurements and get each number of cells
+    """
+    Calculates fraction of cells containing foci from the cell measurements csv file
+    Returns: cells_with_foci / total_cells fraction
+    """
     df = pd.read_csv(csv_path)
     total_cells = len(df)
     cells_with_foci = df['has_aggregate'].sum()
     fraction = cells_with_foci / total_cells if total_cells > 0 else 0
     return fraction
 
-# file paths for each day and mutant, write all of them in order to subtract the baseline values
-# currently doing: series 1, z-slice 1
-day1_paths = ['/Users/nataliaionescu/Documents/PKM2/jobs_results/day1/E3Q_all_data/Series1_Z1_fluorescent.png_cell_measurements.csv', '/Users/nataliaionescu/Documents/PKM2/jobs_results/day1/Q10E_all_data/Series1_Z1_fluorescent_cell_measurements.csv', '/Users/nataliaionescu/Documents/PKM2/jobs_results/day1/WT_all_data/Series1_Z1_fluorescent_cell_measurements.csv']
+# File paths for Series 1
+day1_s1_paths = [
+    '/Users/nataliaionescu/Documents/PKM2/jobs_results/day1/E3Q_all_data/Series1_Z1_fluorescent.png_cell_measurements.csv',
+    '/Users/nataliaionescu/Documents/PKM2/jobs_results/day1/Q10E_all_data/Series1_Z1_fluorescent_cell_measurements.csv', 
+    '/Users/nataliaionescu/Documents/PKM2/jobs_results/day1/WT_all_data/Series1_Z1_fluorescent_cell_measurements.csv'
+]
+day2_s1_paths = [
+    '/Users/nataliaionescu/Documents/PKM2/jobs_results/day2/E3Q_all_data/Series1_Z1_fluorescent_cell_measurements.csv',
+    '/Users/nataliaionescu/Documents/PKM2/jobs_results/day2/Q10E_all_data/Series1_Z1_fluorescent_cell_measurements.csv', 
+    '/Users/nataliaionescu/Documents/PKM2/jobs_results/day2/WT_all_data/Series1_Z1_fluorescent_cell_measurements.csv'
+]
+day3_s1_paths = [
+    '/Users/nataliaionescu/Documents/PKM2/jobs_results/day3/E3Q_all_data/Series1_Z1_fluorescent_cell_measurements.csv',
+    '/Users/nataliaionescu/Documents/PKM2/jobs_results/day3/Q10E_all_data/Series1_Z1_fluorescent_cell_measurements.csv', 
+    '/Users/nataliaionescu/Documents/PKM2/jobs_results/day3/WT_all_data/Series1_Z1_fluorescent_cell_measurements.csv'
+]
+log_s1_paths = [
+    '/Users/nataliaionescu/Documents/PKM2/jobs_results/log/E3Q_all_data/Series1_Z1_fluorescent_cell_measurements.csv',
+    '/Users/nataliaionescu/Documents/PKM2/jobs_results/log/Q10E_all_data/Series1_Z1_fluorescent_cell_measurements.csv', 
+    '/Users/nataliaionescu/Documents/PKM2/jobs_results/log/WT_all_data/Series1_Z1_fluorescent_cell_measurements.csv'
+]
 
-day2_paths = ['/Users/nataliaionescu/Documents/PKM2/jobs_results/day2/E3Q_all_data/Series1_Z1_fluorescent_cell_measurements.csv', '/Users/nataliaionescu/Documents/PKM2/jobs_results/day2/Q10E_all_data/Series1_Z1_fluorescent_cell_measurements.csv', '/Users/nataliaionescu/Documents/PKM2/jobs_results/day2/WT_all_data/Series1_Z1_fluorescent_cell_measurements.csv']
+# File paths for Series 2
+day1_s2_paths = [
+    '/Users/nataliaionescu/Documents/PKM2/jobs_results/day1/E3Q_all_data/Series2_Z1_fluorescent.png_cell_measurements.csv',
+    '/Users/nataliaionescu/Documents/PKM2/jobs_results/day1/Q10E_all_data/Series2_Z1_fluorescent_cell_measurements.csv', 
+    '/Users/nataliaionescu/Documents/PKM2/jobs_results/day1/WT_all_data/Series2_Z1_fluorescent_cell_measurements.csv'
+]
+day2_s2_paths = [
+    '/Users/nataliaionescu/Documents/PKM2/jobs_results/day2/E3Q_all_data/Series2_Z1_fluorescent_cell_measurements.csv',
+    '/Users/nataliaionescu/Documents/PKM2/jobs_results/day2/Q10E_all_data/Series2_Z1_fluorescent_cell_measurements.csv', 
+    '/Users/nataliaionescu/Documents/PKM2/jobs_results/day2/WT_all_data/Series2_Z1_fluorescent_cell_measurements.csv'
+]
+day3_s2_paths = [
+    '/Users/nataliaionescu/Documents/PKM2/jobs_results/day3/E3Q_all_data/Series2_Z1_fluorescent_cell_measurements.csv',
+    '/Users/nataliaionescu/Documents/PKM2/jobs_results/day3/Q10E_all_data/Series2_Z1_fluorescent_cell_measurements.csv', 
+    '/Users/nataliaionescu/Documents/PKM2/jobs_results/day3/WT_all_data/Series2_Z1_fluorescent_cell_measurements.csv'
+]
+log_s2_paths = [
+    '/Users/nataliaionescu/Documents/PKM2/jobs_results/log/E3Q_all_data/Series2_Z1_fluorescent_cell_measurements.csv',
+    '/Users/nataliaionescu/Documents/PKM2/jobs_results/log/Q10E_all_data/Series2_Z1_fluorescent_cell_measurements.csv', 
+    '/Users/nataliaionescu/Documents/PKM2/jobs_results/log/WT_all_data/Series2_Z1_fluorescent_cell_measurements.csv'
+]
 
-day3_paths = ['/Users/nataliaionescu/Documents/PKM2/jobs_results/day3/E3Q_all_data/Series1_Z1_fluorescent_cell_measurements.csv', '/Users/nataliaionescu/Documents/PKM2/jobs_results/day3/Q10E_all_data/Series1_Z1_fluorescent_cell_measurements.csv', '/Users/nataliaionescu/Documents/PKM2/jobs_results/day3/WT_all_data/Series1_Z1_fluorescent_cell_measurements.csv']
+# File paths for Series 3
+day1_s3_paths = [
+    '/Users/nataliaionescu/Documents/PKM2/jobs_results/day1/E3Q_all_data/Series3_Z1_fluorescent.png_cell_measurements.csv',
+    '/Users/nataliaionescu/Documents/PKM2/jobs_results/day1/Q10E_all_data/Series3_Z1_fluorescent_cell_measurements.csv', 
+    '/Users/nataliaionescu/Documents/PKM2/jobs_results/day1/WT_all_data/Series3_Z1_fluorescent_cell_measurements.csv'
+]
+day2_s3_paths = [
+    '/Users/nataliaionescu/Documents/PKM2/jobs_results/day2/E3Q_all_data/Series3_Z1_fluorescent_cell_measurements.csv',
+    '/Users/nataliaionescu/Documents/PKM2/jobs_results/day2/Q10E_all_data/Series3_Z1_fluorescent_cell_measurements.csv', 
+    '/Users/nataliaionescu/Documents/PKM2/jobs_results/day2/WT_all_data/Series3_Z1_fluorescent_cell_measurements.csv'
+]
+day3_s3_paths = [
+    '/Users/nataliaionescu/Documents/PKM2/jobs_results/day3/E3Q_all_data/Series3_Z1_fluorescent_cell_measurements.csv',
+    '/Users/nataliaionescu/Documents/PKM2/jobs_results/day3/Q10E_all_data/Series3_Z1_fluorescent_cell_measurements.csv', 
+    '/Users/nataliaionescu/Documents/PKM2/jobs_results/day3/WT_all_data/Series3_Z1_fluorescent_cell_measurements.csv'
+]
+log_s3_paths = [
+    '/Users/nataliaionescu/Documents/PKM2/jobs_results/log/E3Q_all_data/Series3_Z1_fluorescent_cell_measurements.csv',
+    '/Users/nataliaionescu/Documents/PKM2/jobs_results/log/Q10E_all_data/Series3_Z1_fluorescent_cell_measurements.csv', 
+    '/Users/nataliaionescu/Documents/PKM2/jobs_results/log/WT_all_data/Series3_Z1_fluorescent_cell_measurements.csv'
+]
 
-log_paths = ['/Users/nataliaionescu/Documents/PKM2/jobs_results/log/E3Q_all_data/Series1_Z1_fluorescent_cell_measurements.csv', '/Users/nataliaionescu/Documents/PKM2/jobs_results/log/Q10E_all_data/Series1_Z1_fluorescent_cell_measurements.csv', '/Users/nataliaionescu/Documents/PKM2/jobs_results/log/WT_all_data/Series1_Z1_fluorescent_cell_measurements.csv']   
+# File paths for Series 4
+day1_s4_paths = [
+    '/Users/nataliaionescu/Documents/PKM2/jobs_results/day1/E3Q_all_data/Series4_Z1_fluorescent.png_cell_measurements.csv',
+    '/Users/nataliaionescu/Documents/PKM2/jobs_results/day1/Q10E_all_data/Series4_Z1_fluorescent_cell_measurements.csv', 
+    '/Users/nataliaionescu/Documents/PKM2/jobs_results/day1/WT_all_data/Series4_Z1_fluorescent_cell_measurements.csv'
+]
+day2_s4_paths = [
+    '/Users/nataliaionescu/Documents/PKM2/jobs_results/day2/E3Q_all_data/Series4_Z1_fluorescent_cell_measurements.csv',
+    '/Users/nataliaionescu/Documents/PKM2/jobs_results/day2/Q10E_all_data/Series4_Z1_fluorescent_cell_measurements.csv', 
+    '/Users/nataliaionescu/Documents/PKM2/jobs_results/day2/WT_all_data/Series4_Z1_fluorescent_cell_measurements.csv'
+]
+day3_s4_paths = [
+    '/Users/nataliaionescu/Documents/PKM2/jobs_results/day3/E3Q_all_data/Series4_Z1_fluorescent_cell_measurements.csv',
+    '/Users/nataliaionescu/Documents/PKM2/jobs_results/day3/Q10E_all_data/Series4_Z1_fluorescent_cell_measurements.csv', 
+    '/Users/nataliaionescu/Documents/PKM2/jobs_results/day3/WT_all_data/Series4_Z1_fluorescent_cell_measurements.csv'
+]
+log_s4_paths = [
+    '/Users/nataliaionescu/Documents/PKM2/jobs_results/log/E3Q_all_data/Series4_Z1_fluorescent_cell_measurements.csv',
+    '/Users/nataliaionescu/Documents/PKM2/jobs_results/log/Q10E_all_data/Series4_Z1_fluorescent_cell_measurements.csv', 
+    '/Users/nataliaionescu/Documents/PKM2/jobs_results/log/WT_all_data/Series4_Z1_fluorescent_cell_measurements.csv'
+]
 
 protein_names = ['E3Q', 'Q10E', 'WT']
 colors = ['blue', 'orange', 'green']
 
-# fractions for each day
-day1_fractions = [calculate_foci_fraction(path) for path in day1_paths]
-day2_fractions = [calculate_foci_fraction(path) for path in day2_paths]
-day3_fractions = [calculate_foci_fraction(path) for path in day3_paths]
-log_fractions = [calculate_foci_fraction(path) for path in log_paths]
+# Calculate fractions for each series
+day1_s1_fractions = [calculate_foci_fraction(path) for path in day1_s1_paths]
+day2_s1_fractions = [calculate_foci_fraction(path) for path in day2_s1_paths]
+day3_s1_fractions = [calculate_foci_fraction(path) for path in day3_s1_paths]
+log_s1_fractions = [calculate_foci_fraction(path) for path in log_s1_paths]
 
-# plot day 1 
-plt.figure(figsize=(8,6))
-bars = plt.bar(protein_names, day1_fractions, color=colors, edgecolor='black')
-x = range(len(protein_names))
-plt.xlabel('Protein')
-plt.ylabel('Fraction of cells containing foci')
-ax = plt.gca()
-plt.title('Day 1: Fraction of cells containing foci')
-plt.xticks(x, protein_names)
+day1_s2_fractions = [calculate_foci_fraction(path) for path in day1_s2_paths]
+day2_s2_fractions = [calculate_foci_fraction(path) for path in day2_s2_paths]
+day3_s2_fractions = [calculate_foci_fraction(path) for path in day3_s2_paths]
+log_s2_fractions = [calculate_foci_fraction(path) for path in log_s2_paths]
+
+day1_s3_fractions = [calculate_foci_fraction(path) for path in day1_s3_paths]
+day2_s3_fractions = [calculate_foci_fraction(path) for path in day2_s3_paths]
+day3_s3_fractions = [calculate_foci_fraction(path) for path in day3_s3_paths]
+log_s3_fractions = [calculate_foci_fraction(path) for path in log_s3_paths]
+
+day1_s4_fractions = [calculate_foci_fraction(path) for path in day1_s4_paths]
+day2_s4_fractions = [calculate_foci_fraction(path) for path in day2_s4_paths]
+day3_s4_fractions = [calculate_foci_fraction(path) for path in day3_s4_paths]
+log_s4_fractions = [calculate_foci_fraction(path) for path in log_s4_paths]
+
+# Series 1 - 2x2 plot
+fig, axes = plt.subplots(2, 2, figsize=(12, 10))
+axes[0,0].bar(protein_names, day1_s1_fractions, color=colors, edgecolor='black')
+axes[0,0].set_title('Day 1 - Series 1')
+axes[0,1].bar(protein_names, day2_s1_fractions, color=colors, edgecolor='black')
+axes[0,1].set_title('Day 2 - Series 1')
+axes[1,0].bar(protein_names, day3_s1_fractions, color=colors, edgecolor='black')
+axes[1,0].set_title('Day 3 - Series 1')
+axes[1,1].bar(protein_names, log_s1_fractions, color=colors, edgecolor='black')
+axes[1,1].set_title('Log - Series 1')
+plt.suptitle('Series 1: Fraction of cells with foci')
 plt.tight_layout()
 plt.show()
 
-# plot day 2
-plt.figure(figsize=(8,6))
-bars = plt.bar(protein_names, day2_fractions, color=colors, edgecolor='black')
-x = range(len(protein_names))
-plt.xlabel('Protein')
-plt.ylabel('Fraction of cells containing foci')
-ax = plt.gca()
-plt.title('Day 2: Fraction of cells containing foci')
-plt.xticks(x, protein_names)
+# Series 2 - 2x2 plot
+fig, axes = plt.subplots(2, 2, figsize=(12, 10))
+axes[0,0].bar(protein_names, day1_s2_fractions, color=colors, edgecolor='black')
+axes[0,0].set_title('Day 1 - Series 2')
+axes[0,1].bar(protein_names, day2_s2_fractions, color=colors, edgecolor='black')
+axes[0,1].set_title('Day 2 - Series 2')
+axes[1,0].bar(protein_names, day3_s2_fractions, color=colors, edgecolor='black')
+axes[1,0].set_title('Day 3 - Series 2')
+axes[1,1].bar(protein_names, log_s2_fractions, color=colors, edgecolor='black')
+axes[1,1].set_title('Log - Series 2')
+plt.suptitle('Series 2: Fraction of cells with foci')
 plt.tight_layout()
 plt.show()
 
-# plot day 3
-plt.figure(figsize=(8,6))
-bars = plt.bar(protein_names, day3_fractions, color=colors, edgecolor='black')
-x = range(len(protein_names))
-plt.xlabel('Protein')
-plt.ylabel('Fraction of cells containing foci')
-ax = plt.gca()
-plt.title('Day 3: Fraction of cells containing foci')
-plt.xticks(x, protein_names)
+# Series 3 - 2x2 plot
+fig, axes = plt.subplots(2, 2, figsize=(12, 10))
+axes[0,0].bar(protein_names, day1_s3_fractions, color=colors, edgecolor='black')
+axes[0,0].set_title('Day 1 - Series 3')
+axes[0,1].bar(protein_names, day2_s3_fractions, color=colors, edgecolor='black')
+axes[0,1].set_title('Day 2 - Series 3')
+axes[1,0].bar(protein_names, day3_s3_fractions, color=colors, edgecolor='black')
+axes[1,0].set_title('Day 3 - Series 3')
+axes[1,1].bar(protein_names, log_s3_fractions, color=colors, edgecolor='black')
+axes[1,1].set_title('Log - Series 3')
+plt.suptitle('Series 3: Fraction of cells with foci')
 plt.tight_layout()
 plt.show()
 
-# plot log
-plt.figure(figsize=(8,6))
-bars = plt.bar(protein_names, log_fractions, color=colors, edgecolor='black')
-x = range(len(protein_names))
-plt.xlabel('Protein')
-plt.ylabel('Fraction of cells containing foci')
-ax = plt.gca()
-plt.title('Log: Fraction of cells containing foci')
-plt.xticks(x, protein_names)
+# Series 4 - 2x2 plot
+fig, axes = plt.subplots(2, 2, figsize=(12, 10))
+axes[0,0].bar(protein_names, day1_s4_fractions, color=colors, edgecolor='black')
+axes[0,0].set_title('Day 1 - Series 4')
+axes[0,1].bar(protein_names, day2_s4_fractions, color=colors, edgecolor='black')
+axes[0,1].set_title('Day 2 - Series 4')
+axes[1,0].bar(protein_names, day3_s4_fractions, color=colors, edgecolor='black')
+axes[1,0].set_title('Day 3 - Series 4')
+axes[1,1].bar(protein_names, log_s4_fractions, color=colors, edgecolor='black')
+axes[1,1].set_title('Log - Series 4')
+plt.suptitle('Series 4: Fraction of cells with foci')
 plt.tight_layout()
 plt.show()
 
-# get differences day - log
-day1_diff = [day1_fractions[i] - log_fractions[i] for i in range(3)]
-day2_diff = [day2_fractions[i] - log_fractions[i] for i in range(3)]
-day3_diff = [day3_fractions[i] - log_fractions[i] for i in range(3)]
+# Calculate averages across all 4 series
+day1_avg = [(day1_s1_fractions[i] + day1_s2_fractions[i] + day1_s3_fractions[i] + day1_s4_fractions[i])/4 for i in range(3)]
+day2_avg = [(day2_s1_fractions[i] + day2_s2_fractions[i] + day2_s3_fractions[i] + day2_s4_fractions[i])/4 for i in range(3)]
+day3_avg = [(day3_s1_fractions[i] + day3_s2_fractions[i] + day3_s3_fractions[i] + day3_s4_fractions[i])/4 for i in range(3)]
+log_avg = [(log_s1_fractions[i] + log_s2_fractions[i] + log_s3_fractions[i] + log_s4_fractions[i])/4 for i in range(3)]
 
-# plot day 1 - log
-plt.figure(figsize=(8,6))
-bars = plt.bar(protein_names, day1_diff, color=colors, edgecolor='black')
-x = range(len(protein_names))
-plt.xlabel('Protein')
-plt.ylabel('Fraction of cells containing foci')
-ax = plt.gca()
-plt.title('Day 1: Fraction of cells containing foci, subtracting baseline values')
-plt.xticks(x, protein_names)
+# Average 2x2 plot
+fig, axes = plt.subplots(2, 2, figsize=(12, 10))
+axes[0,0].bar(protein_names, day1_avg, color=colors, edgecolor='black')
+axes[0,0].set_title('Day 1 - Average')
+axes[0,1].bar(protein_names, day2_avg, color=colors, edgecolor='black')
+axes[0,1].set_title('Day 2 - Average')
+axes[1,0].bar(protein_names, day3_avg, color=colors, edgecolor='black')
+axes[1,0].set_title('Day 3 - Average')
+axes[1,1].bar(protein_names, log_avg, color=colors, edgecolor='black')
+axes[1,1].set_title('Log - Average')
+plt.suptitle('AVERAGE: Fraction of cells with foci (across all 4 series)')
 plt.tight_layout()
 plt.show()
 
-# plot day 2 - log
+# Calculate differences from log (average)
+day1_diff_avg = [day1_avg[i] - log_avg[i] for i in range(3)]
+day2_diff_avg = [day2_avg[i] - log_avg[i] for i in range(3)]
+day3_diff_avg = [day3_avg[i] - log_avg[i] for i in range(3)]
+
+# Differences plot
 plt.figure(figsize=(8,6))
-bars = plt.bar(protein_names, day2_diff, color=colors, edgecolor='black')
-x = range(len(protein_names))
+plt.bar(protein_names, day1_diff_avg, color=colors, edgecolor='black')
+plt.axhline(y=0, color='black', linestyle='-', linewidth=0.8)
 plt.xlabel('Protein')
-plt.ylabel('Fraction of cells containing foci')
-ax = plt.gca()
-plt.title('Day 2: Fraction of cells containing foci, subtracting baseline values')
-plt.xticks(x, protein_names)
-plt.tight_layout()
+plt.ylabel('Difference in fraction (Day 1 - Log)')
+plt.title('AVERAGE: Day 1 - Log (across all 4 series)')
 plt.show()
 
-# plot day 3 - log
 plt.figure(figsize=(8,6))
-bars = plt.bar(protein_names, day3_diff, color=colors, edgecolor='black')
-x = range(len(protein_names))
+plt.bar(protein_names, day2_diff_avg, color=colors, edgecolor='black')
+plt.axhline(y=0, color='black', linestyle='-', linewidth=0.8)
 plt.xlabel('Protein')
-plt.ylabel('Fraction of cells containing foci')
-ax = plt.gca()
-plt.title('Day 3: Fraction of cells containing foci, subtracting baseline values')
-plt.xticks(x, protein_names)
-plt.tight_layout()
+plt.ylabel('Difference in fraction (Day 2 - Log)')
+plt.title('AVERAGE: Day 2 - Log (across all 4 series)')
 plt.show()
 
+plt.figure(figsize=(8,6))
+plt.bar(protein_names, day3_diff_avg, color=colors, edgecolor='black')
+plt.axhline(y=0, color='black', linestyle='-', linewidth=0.8)
+plt.xlabel('Protein')
+plt.ylabel('Difference in fraction (Day 3 - Log)')
+plt.title('AVERAGE: Day 3 - Log (across all 4 series)')
+plt.show()
+
+print("Average results:")
+print("Day 1 avg:", [f"{f:.3f}" for f in day1_avg])
+print("Day 2 avg:", [f"{f:.3f}" for f in day2_avg])
+print("Day 3 avg:", [f"{f:.3f}" for f in day3_avg])
+print("Log avg:", [f"{f:.3f}" for f in log_avg])
 
 
 
